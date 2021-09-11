613f6fc148000fe2c0690ae5c441f693fba30b3a
rishiamarnath1 <rishiamarnath1@gmail.com>
2021-09-11 16:52:28 -0500

    graph included

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './home';
+import Graph from './graph';
 
 function App() {
   return <div className="App">
       <Home />
+      <p><Graph /> </p>
+      
     </div>;
     // <div className="App">
     //   <header className="App-header">
